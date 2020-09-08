b7f90e5b81b810a25b9c2de1b62b9e0cdc80ec54
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-09-08 16:26:19 -0700

    initial commit

---
+++ stamp_program.py
@@ -0,0 +1,20 @@
+
+
+"""
+start
+get the numbers of sheets
+sheets / 5
+round answer to next number
+output the result to the user
+end
+"""
+
+sheet = 16
+
+answer = sheet / 5
+
+round = round(answer)
+print("sheet is : ", sheet)
+print("The answer is: ", answer)
+print("rounded is: ", rounded)
+# answer = rount(sheet / 5)
